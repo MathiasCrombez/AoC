2d6cf48d0843c87ecd3e6d454b899532f982aa4f
Mathias Crombez <mathias.crombez@qorvo.com>
2026-02-13 13:49:17 +0100

    optim day 2

--- rust/src/aoc2.rs
+++ rust/src/aoc2.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use crate::helpers::Part;
 
 pub const RESULTS: [[i64; 2]; 2] = [
@@ -7,27 +6,25 @@ pub const RESULTS: [[i64; 2]; 2] = [
 ];
 
 pub fn parse_input(input: &str, mode: &Part) -> i64 {
-
-    let re = Regex::new(r"(\d+)-(\d+)").unwrap();
     let mut result: i64 = 0;
 
-    for (_, [start_str, end_str]) in re.captures_iter(input).filter_map(|cap| {
-        Some((cap.get(0).unwrap().as_str(), [cap.get(1).unwrap().as_str(), cap.get(2).unwrap().as_str()]))
-    }) {
-        let start: i64 = start_str.parse().unwrap();
-        let end: i64 = end_str.parse().unwrap();
+    for range in input.split(',').map(|s| s.trim()) {
+        let (start, end) = range
+            .split_once('-')
+            .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap())).unwrap_or((0, 0));
 
         for num in start..=end {
             let num_str = num.to_string();
-            let len = num_str.len();
+            let num_bytes = num_str.as_bytes();
+            let len = num_bytes.len();
             match mode {
                 Part::Part1 => {
                     if len % 2 != 0 {
                         continue; // Skip numbers with odd length
                     }
                     let mid = len / 2;
-                    let first_half = &num_str[..mid];
-                    let second_half = &num_str[mid..];
+                    let first_half = &num_bytes[..mid];
+                    let second_half = &num_bytes[mid..];
                     if first_half == second_half {
                         result += num;
                     }
@@ -38,10 +35,11 @@ pub fn parse_input(input: &str, mode: &Part) -> i64 {
                             continue;
                         }
                         // Check if the number consists of repeating segments
-                        let segment_bytes = &num_str.as_bytes()[..i];
+                        let segment_bytes = &num_bytes[..i];
                         // Convert the string in bytes slice for comparison and check all slices
-                        let all_match = num_str.as_bytes()
-                            .chunks_exact(i)
+                        let all_match = num_bytes
+                            .chunks(i)
+                            .skip(1)
                             .all(|chunk| chunk == segment_bytes);
                         
                         if all_match {
